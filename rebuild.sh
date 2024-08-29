97ceeb20a1ac61197d3ee4c7101c0f0f9cd175be
kareem <safariknight2@gmail.com>
2024-08-29 22:23:30 +0300

    rebuild.sh: apparently I don't know how to do default values

--- rebuild.sh
+++ rebuild.sh
@@ -1,1 +1,3 @@
-sudo nixos-rebuild switch --flake $(pwd)#{$1:-desktop} --option eval-cache false --show-trace $2
+FLAKE_NAME=${1:-"desktop"}
+
+sudo nixos-rebuild switch --flake $(pwd)#$FLAKE_NAME --option eval-cache false $2
