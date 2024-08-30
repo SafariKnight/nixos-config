03188d6024f64fe915ca3658e304084dd7b93ae0
kareem <safariknight2@gmail.com>
2024-08-30 03:24:09 +0300

    users kareem.nix: changed default cursor to posy     The default on hyprland is just so ugly man

--- users/kareem.nix
+++ users/kareem.nix
@@ -4,16 +4,26 @@
   imports = [
     ./../modules/home-manager
   ];
-  home.username = "kareem";
-  home.homeDirectory = "/home/kareem";
   programs.home-manager.enable = true;
-  home.stateVersion = "24.05";
+  home = {
+    username = "kareem";
+    homeDirectory = "/home/kareem";
+    stateVersion = "24.05";
 
-  home.packages = [ ];
-  home.file = { };
-  home.sessionVariables = {
-    HYPRCURSOR_SIZE = 32;
-    XCURSOR_SIZE = 32;
-    QT_QPA_PLATFORMTHEME = "qt6ct";
+    packages = [ ];
+    file = { };
+    sessionVariables = {
+      HYPRCURSOR_SIZE = 32;
+      XCURSOR_SIZE = 32;
+      QT_QPA_PLATFORMTHEME = "qt6ct";
+    };
+
+    pointerCursor = {
+      name = "Posy_Cursor_Black";
+      size = 32;
+      package = pkgs.posy-cursors;
+      x11.enable = true;
+      gtk.enable = true;
+    };
   };
 }
