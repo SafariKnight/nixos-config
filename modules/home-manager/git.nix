2f9754c3835a051f1dc5c7538286675a5bfe4b8c
kareem <safariknight2@gmail.com>
2024-08-31 14:50:32 +0300

    git: added some aliases     how tf did I live without these

--- modules/home-manager/git.nix
+++ modules/home-manager/git.nix
@@ -2,10 +2,15 @@
 {
   programs.git = {
     enable = true;
-    userName = "kareem";
+    userName = "Kareem Hisham";
     userEmail = "safariknight2@gmail.com";
     extraConfig = {
       init.defaultBranch = "main";
+    };
+    aliases = {
+      l = "log --oneline";
+      s = "status -s";
+      co = "checkout";
     };
   };
 }
