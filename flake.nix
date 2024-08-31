f483dcf84355b596efb6478db934fb09acabe5ae
kareem <safariknight2@gmail.com>
2024-08-31 11:35:04 +0300

    flake.nix: nixfmt

--- flake.nix
+++ flake.nix
@@ -1,25 +1,25 @@
 {
   description = "My NixOS config";
   inputs = {
-      nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-      nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.05";
 
-      home-manager = {
-        url = "github:nix-community/home-manager";
-        inputs.nixpkgs.follows = "nixpkgs";
-      };
-      nur.url = "github:nix-community/NUR";
-      nixvim = {
-        url = "github:nix-community/nixvim";
-        inputs.nixpkgs.follows = "nixpkgs";
-      };
-      ags.url = "github:Aylur/ags";
-      ags.inputs.nixpkgs.follows = "nixpkgs";
-      stylix = {
-        url = "github:danth/stylix";
-        inputs.nixpkgs.follows = "nixpkgs";
-      };
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
+    nur.url = "github:nix-community/NUR";
+    nixvim = {
+      url = "github:nix-community/nixvim";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    ags.url = "github:Aylur/ags";
+    ags.inputs.nixpkgs.follows = "nixpkgs";
+    stylix = {
+      url = "github:danth/stylix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
 
   outputs =
     {
@@ -33,6 +33,9 @@
       mkSystem = import ./lib/mksystem.nix { inherit nixpkgs inputs outputs; };
     in
     {
+      nixConfig = {
+        warn-dirty = false;
+      };
       overlays = import ./overlays { inherit inputs; };
       nixosConfigurations = {
         desktop = mkSystem "desktop" {
