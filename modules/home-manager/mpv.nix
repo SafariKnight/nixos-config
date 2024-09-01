3540ac2c5ea85a9bd2d1c797ae111f4ff46c192d
Kareem Hisham <safariknight2@gmail.com>
2024-09-01 15:37:39 +0300

    173 current  2024-09-01 15:37:29  24.11.20240828.71e91c4   6.10.6                          *

--- modules/home-manager/mpv.nix
+++ modules/home-manager/mpv.nix
@@ -4,26 +4,26 @@
   programs.mpv = {
     enable = true;
     scripts = with pkgs.mpvScripts; [
-      uosc
+      # uosc
       thumbfast
     ];
     config = {
-      profile = "gpu-hq";
-      # gpu-api = "vulkan";
+      # profile = "gpu-hq";
+      gpu-api = "vulkan";
 
-      deband = "yes";
-      deband-iterations = 2;
-
-      deband-threshold = 35;
-      deband-range = 20;
-      deband-grain = 5;
-
-      dither-depth = "auto";
+      # deband = "yes";
+      # deband-iterations = 2;
+      #
+      # deband-threshold = 35;
+      # deband-range = 20;
+      # deband-grain = 5;
+      #
+      # dither-depth = "auto";
 
       # Scaling
-      # scale = "ewa_lanczossharp";
-      # dscale = "mitchell";
-      # cscale = "spline64";
+      scale = "ewa_lanczossharp";
+      dscale = "mitchell";
+      cscale = "spline64";
 
       ## Behavior (personal preference)
       keep-open = "yes";
@@ -45,27 +45,27 @@
       slang = "en,eng,de,deu,ger";
 
       # # Subs
-      sub-ass-override = "yes";
-      sub-ass-style-overrides = [
-        "playresx=1920,playresy=1080"
-        "Kerning=yes"
-      ];
-      sub-bold = "yes";
-      sub-font = "Gandhi Sans";
-      sub-font-size = 39;
-      sub-blur = 0.1;
-      sub-color = "1.0/1.0/1.0/1.0";
-      # sub-border-color=0.0/0.0/0.0/1.0
-      sub-border-size = 1;
-      # sub-border-offset=2
-      # sub-margin-x=100
-      # sub-margin-y=50
-      sub-shadow-color = "0.0/0.0/0.0/0.5";
-      sub-shadow-offset = 0.2;
-
-      osc = "no";
-      cursor-autohide = 3500;
-      osd-bar = "no";
+      # sub-ass-override = "yes";
+      # sub-ass-style-overrides = [
+      #   "playresx=1920,playresy=1080"
+      #   "Kerning=yes"
+      # ];
+      # sub-bold = "yes";
+      # sub-font = "Gandhi Sans";
+      # sub-font-size = 39;
+      # sub-blur = 0.1;
+      # sub-color = "1.0/1.0/1.0/1.0";
+      # # sub-border-color=0.0/0.0/0.0/1.0
+      # sub-border-size = 1;
+      # # sub-border-offset=2
+      # # sub-margin-x=100
+      # # sub-margin-y=50
+      # sub-shadow-color = "0.0/0.0/0.0/0.5";
+      # sub-shadow-offset = 0.2;
+      #
+      # osc = "no";
+      # cursor-autohide = 3500;
+      # osd-bar = "no";
 
       # # External
       ytdl-format = "bestvideo[height<=?480][vcodec!=?vp9]+bestaudio/best";
