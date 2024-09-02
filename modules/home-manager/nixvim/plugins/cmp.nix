3f067c290d23d3970fc8005d60424e16b74c9128
Kareem Hisham <safariknight2@gmail.com>
2024-09-03 01:39:33 +0300

    190 current  2024-09-03 01:37:49  24.11.20240828.71e91c4   6.10.5-zen1                          *

--- modules/home-manager/nixvim/plugins/cmp.nix
+++ modules/home-manager/nixvim/plugins/cmp.nix
@@ -15,7 +15,11 @@
         sources = [
           { name = "nvim_lsp"; }
           { name = "path"; }
-          { name = "buffer"; }
+          {
+            name = "buffer"; # text within current buffer
+            option.get_bufnrs.__raw = "vim.api.nvim_list_bufs";
+            keywordLength = 3;
+          }
         ];
         snippet.expand.__raw = ''
           function(args)
