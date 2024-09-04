1699950ace3774aafbe25b521d579855b952d8e0
Kareem Hisham <safariknight2@gmail.com>
2024-09-04 13:49:52 +0300

    nixvim: enabled tsserver

--- modules/home-manager/nixvim/plugins/typescript.nix
+++ modules/home-manager/nixvim/plugins/typescript.nix
@@ -5,6 +5,7 @@
       enable = true;
       settings.tsserverPlugins = [ "@vue/typescript-plugin" ];
     };
+    lsp.servers.tsserver.enable = true;
     lsp.servers.tsserver.filetypes = [
       "javascript"
       "javascriptreact"
