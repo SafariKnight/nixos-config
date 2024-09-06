b2048fd407aec7eec0627afc2a49650a4d75b3c0
Kareem Hisham <safariknight2@gmail.com>
2024-09-07 01:51:24 +0300

    refactor: volar needs an env variable for tsserver
    I believed this was the best way to do this

--- dotfiles/nvim/lua/plugins/lsp/servers/volar.lua
+++ dotfiles/nvim/lua/plugins/lsp/servers/volar.lua
@@ -1,15 +1,20 @@
 -- local tsserver = require("mason-registry")
 --   .get_package("typescript-language-server")
 --   :get_install_path() .. "/node_modules/typescript/lib"
+local tsserver = os.getenv("TSSERVERJS")
 
+-- print(tsserver)
 require("lspconfig").volar.setup({
   capabilities = _G.capabilities,
   on_attach = require("plugins.lsp.on_attach"),
-  -- init_options = {
-  --   typescript = {
-  --     tsdk = tsserver,
-  --   },
-  -- },
+  init_options = {
+    typescript = {
+      tsdk = tsserver,
+    },
+    vue = {
+      hybridMode = true,
+    },
+  },
 })
     -- vue = {
     --   hybridMode = true,
