a38695b0b7d814d9bfbdb08705cbc14e5effee33
Kareem Hisham <safariknight2@gmail.com>
2024-09-19 19:25:36 +0300

    feature: nixos gaming module

--- modules/nixos/gaming.nix
+++ modules/nixos/gaming.nix
@@ -11,10 +11,48 @@
     programs.steam.enable = true;
     programs.steam.protontricks.enable = true;
     environment.systemPackages = with pkgs; [
-      lutris
+      (lutris.override {
+        extraPkgs = pkgs:
+          with pkgs; [
+            dolphin-emu
+            dolphin-emu-primehack
+            pcsx2
+            ppsspp
+            duckstation
+            bsnes-hd
+            ryujinx
+            mgba
+            flycast
+            melonDS
+            gamescope
+            gamemode
+            mangohud
+          ];
+      })
       wine
       wine64
       winetricks
+      appimage-run
+      dolphin-emu
+      dolphin-emu-primehack
+      pcsx2
+      ppsspp
+      duckstation
+      bsnes-hd
+      ryujinx
+      mgba
+      flycast
+      melonDS
+      # (osu-lazer-bin.overrideAttrs {
+      #   # version = "2024.906.2";
+      #   src = fetchurl {
+      #     url = "https://github.com/ppy/osu/releases/download/2024.906.2/osu.AppImage";
+      #     curlOpts = "-L";
+      #     # hash = "sha256-zQnR3KwlE1gTWH8f+GDRBsc7Whfn9XpT1D/NLg5TtrU=";
+      #   };
+      # })
+      (retroarch.override {cores = [pkgs.libretro.parallel-n64];})
+      libretro.parallel-n64
     ];
   };
 }
