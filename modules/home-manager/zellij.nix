11a2a2cddc34206b1866b2ba0b585b26f2c2fe3d
Kareem Hisham <safariknight2@gmail.com>
2024-09-28 15:28:14 +0300

    feature: enable zellij

--- modules/home-manager/zellij.nix
+++ modules/home-manager/zellij.nix
@@ -1,4 +1,5 @@
 {config, ...}: {
+  stylix.targets.zellij.enable = false;
   programs.zellij.enable = true;
   xdg.configFile.zellij.source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nixos-config/dotfiles/zellij/";
 }
