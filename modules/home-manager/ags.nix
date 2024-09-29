d65f5d65f9b4e02e3414d2c0e1708d900d4f8f4e
Kareem Hisham <safariknight2@gmail.com>
2024-09-29 21:00:07 +0300

    cleanup: move ags to dotfiles

--- modules/home-manager/ags.nix
+++ modules/home-manager/ags.nix
@@ -1,6 +1,7 @@
 {
   inputs,
   pkgs,
+  config,
   ...
 }: {
   imports = [inputs.ags.homeManagerModules.default];
@@ -8,7 +9,7 @@
     enable = true;
 
     # null or path, leave as null if you don't want hm to manage the config
-    configDir = ./config;
+    configDir = config.lib.file.mkOutOfStoreSymlink ./../../dotfiles/ags;
 
     # additional packages to add to gjs's runtime
     extraPackages = with pkgs; [
