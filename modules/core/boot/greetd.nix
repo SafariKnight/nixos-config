1ec891e7282d871a449af62ab101dfe6e0e40f27
Kareem Hisham <safariknight2@gmail.com>
2025-01-16 10:53:18 +0200

    feat(greetd): use tuigreet over agreety

--- modules/core/boot/greetd.nix
+++ modules/core/boot/greetd.nix
@@ -1,19 +1,33 @@
 {
   config,
   lib,
+  pkgs,
   ...
-}: {
-  config = lib.mkIf config.modules.boot.greetd.enable {
+}: let
+  cfg = config.modules.boot.greetd;
+in {
+  config = lib.mkIf cfg.enable {
     services.greetd = {
       enable = true;
       settings = {
-        default_session = {
-          command = "$agreety --cmd bash";
-          user = "${config.modules.boot.greetd.startupUser}";
-        };
-        initial_session = {
-          command = "${config.modules.boot.greetd.command} >/dev/null";
-          user = "${config.modules.boot.greetd.startupUser}";
+        default_session = let
+          tuigreet = "${lib.getExe pkgs.greetd.tuigreet}";
+          baseSessions = "${config.services.displayManager.sessionData.desktops}";
+          xSessions = "${baseSessions}/share/xsessions";
+          waylandSessions = "${baseSessions}/share/wayland-sessions";
+          tuigreetOptions = [
+            "--remember"
+            "--remember-session"
+            "--sessions ${waylandSessions}:${xSessions}"
+            "--time"
+            # Make sure theme is wrapped in single quotes. See https://github.com/apognu/tuigreet/issues/147
+            "--theme 'border=magenta;text=cyan;prompt=green;time=red;action=blue;button=yellow;container=black;input=red'"
+            "--cmd ${cfg.command}"
+          ];
+          flags = lib.concatStringsSep " " tuigreetOptions;
+        in {
+          command = "${tuigreet} ${flags}";
+          user = "greeter";
         };
       };
     };
