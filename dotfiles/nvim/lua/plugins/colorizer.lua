26007ec603e2e1cc4304a17c3b0b50e51012d37a
Kareem Hisham <safariknight2@gmail.com>
2025-02-06 01:41:39 +0200

    feat(nvim): colorizer

---
+++ dotfiles/nvim/lua/plugins/colorizer.lua
@@ -0,0 +1,10 @@
+return {
+  "catgoose/nvim-colorizer.lua",
+  event = "BufReadPre",
+  opts = { -- set to setup table
+    user_default_options = {
+      names = false;
+      css_fn = true,
+    },
+  },
+}
