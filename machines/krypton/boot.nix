871991f97d5310a92ab4841d777ec1c0fe6cb77b
Kareem Hisham <safariknight2@gmail.com>
2025-02-08 09:23:02 +0200

    feat: psp via usb support
    I don't actually know if these allow it to work, but it's worth a try

--- machines/krypton/boot.nix
+++ machines/krypton/boot.nix
@@ -5,18 +5,27 @@
     extraModulePackages = with config.boot.kernelPackages; [
       v4l2loopback
     ];
+    supportedFilesystems = [ "ntfs" ];
     extraModprobeConfig = ''
       options iwlmvm  power_scheme=1
       options iwlwifi power_save=0
+      options usbcore use_both_schemes=y
     '';
-    kernelModules = [ "v4l2loopback" ];
+    kernelModules = [
+      "v4l2loopback"
+      "usbcore"
+      "uas"
+      "usb_storage"
+    ];
     initrd = {
       kernelModules = [ "kvm-amd" ];
       availableKernelModules = [
         "xhci_pci"
         "ahci"
-        "usb_storage"
         "usbhid"
+        "uas"
+        "usbcore"
+        "usb_storage"
         "sd_mod"
       ];
     };
